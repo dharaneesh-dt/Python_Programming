62ef97e42344db46931c41bebc5f2a650419a675
dharaneesh-dt <152013506+dharaneesh-dt@users.noreply.github.com>
2023-11-28 23:20:50 +0530

    Python numbers
    Python numbers with random and few more

---
+++ 03_pythonNumbers.py
@@ -0,0 +1,28 @@
+x=10
+y=-364
+z=423e5
+a=3+5j
+print("the value of  X is : ",x)
+print("the value of Y is : ",y)
+print("the value of Z is : ",z)
+print("the value of A is : ",a)
+print()
+print()
+print("The type of x is : ",type(x))
+print("The type of y is : ",type(y))
+print("The type of z is : ",type(z))
+print("The type of a is : ",type(a))
+print()
+print()
+#Type conversion
+#convert from int to float
+print("The value of float for int x is :",float(x))
+#convert from float to int
+print("The value of int for float z is :",int(z))
+#convert from int to complex
+print("The value od complex for int x is :",complex(x))
+print()
+print()
+#Random number
+import random
+print("The random value between 1 to 10 is :",random.randrange(1,10,))
